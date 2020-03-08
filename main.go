9eb40a98773ce72674b27ab25600af4bb72aae0c
sylvioneto <sylvio.pedroza@gmail.com>
2020-03-08 14:14:10 -0600

    create function to withdraw

--- main.go
+++ main.go
@@ -6,24 +6,35 @@ type CurrentAccount struct {
 	customerName string
 	branchNo     string
 	accountNo    string
-	balance      float32
+	balance      float64
 }
 
 func main() {
 	sylvioAccount := CurrentAccount{customerName: "sylvio", branchNo: "0001", accountNo: "12345", balance: 100.01}
 	fmt.Println(sylvioAccount)
 
-	netoAccount := CurrentAccount{"neto", "0001", "6789", 200.02}
-	fmt.Println(netoAccount)
+	err := sylvioAccount.Withdraw(50)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(sylvioAccount)
 
-	var johnAccount *CurrentAccount
-	johnAccount = new(CurrentAccount)
-	johnAccount.customerName = "john"
-	fmt.Println(*johnAccount)
+	err = sylvioAccount.Withdraw(100)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(sylvioAccount)
 
-	sylvioAccount2 := CurrentAccount{customerName: "sylvio", branchNo: "0001", accountNo: "12345", balance: 100.01}
-	fmt.Println(sylvioAccount2)
+}
 
-	// compare two accounts with same values
-	fmt.Println(sylvioAccount == sylvioAccount2)
+// Withdraw validates balance and update it
+func (c *CurrentAccount) Withdraw(amount float64) error {
+	var err error
+	hasBalance := c.balance >= amount && amount > 0
+	if hasBalance {
+		c.balance -= amount
+	} else {
+		err = fmt.Errorf("Insuficient balance: %f", c.balance)
+	}
+	return err
 }
