db1bdac6495596b3adc1ed569403edb76f7e04c7
sylvioneto <sylvio.pedroza@gmail.com>
2020-03-08 15:12:05 -0600

    add deposit method

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type CurrentAccount struct {
 	customerName string
@@ -25,16 +27,45 @@ func main() {
 	}
 	fmt.Println(sylvioAccount)
 
+	err = sylvioAccount.Withdraw(-50)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(sylvioAccount)
+
+	err = sylvioAccount.Deposit(-50)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(sylvioAccount)
+
+	err = sylvioAccount.Deposit(100)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(sylvioAccount)
 }
 
-// Withdraw validates balance and update it
+// Withdraw validates balance and updates it
 func (c *CurrentAccount) Withdraw(amount float64) error {
 	var err error
 	hasBalance := c.balance >= amount && amount > 0
 	if hasBalance {
 		c.balance -= amount
 	} else {
-		err = fmt.Errorf("Insuficient balance: %f", c.balance)
+		err = fmt.Errorf("Insuficient balance: %.2f, or Invalid amount %.2f", c.balance, amount)
+	}
+	return err
+}
+
+// Deposit validates amount and updates the deposit
+func (c *CurrentAccount) Deposit(amount float64) error {
+	var err error
+	if amount > 0 {
+		c.balance += amount
+		fmt.Println("New balance is", c.balance)
+	} else {
+		err = fmt.Errorf("Invalid deposit amount: %.2f", amount)
 	}
 	return err
 }
