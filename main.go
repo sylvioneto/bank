06e6f49b7be0dd2825a44e8543b48f5ca8fd5745
sylvioneto <sylvio.pedroza@gmail.com>
2020-03-08 12:53:36 -0600

    using struct

--- main.go
+++ main.go
@@ -10,5 +10,9 @@ type CurrentAccount struct {
 }
 
 func main() {
-	fmt.Println(CurrentAccount{})
+	sylvioAccount := CurrentAccount{customerName: "sylvio", branchNo: "0001", accountNo: "12345", balance: 100.01}
+	fmt.Println(sylvioAccount)
+
+	netoAccount := CurrentAccount{"neto", "0001", "6789", 200.02}
+	fmt.Println(netoAccount)
 }
